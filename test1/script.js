e80efc9ac11fc5fb5b85618a47ed4c4d3992bd4c
NAMRA313 <armandkouadio741@gmail.com>
2026-02-04 14:26:11 +0000

    Delete test1/script.js

--- test1/script.js
+++
@@ -1,6 +0,0 @@
-function toggleMode() {
-    document.body.classList.toggle('dark-mode');
-    const msg = document.getElementById('msg');
-    msg.innerText = "Interaction JS réussie ! Le thème a été modifié.";
-    msg.classList.add('show');
-}
